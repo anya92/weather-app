7e7ee7b15363af10e6a402035265862e0b0b60a2
Ania Brajtkopf <aniabrajtkopf@gmail.com>
2017-04-24 12:01:35 +0200

    get weather

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import fetchJsonp from 'fetch-jsonp';
+import { API_KEY } from '../config';
 
 class App extends Component {
   constructor() {
@@ -12,27 +14,53 @@ class App extends Component {
   }
 
   componentWillMount() {
-    let lat = null, lon = null;
+    this.getLocation();
+  }
+
+  getLocation() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(position => {
-        lat = position.coords.latitude;
-        lon = position.coords.longitude;
         this.setState({
           location: {
-            lat,
-            lon
+            lat: position.coords.latitude,
+            lon: position.coords.longitude
           }
         });
       });
     } else {
       this.setState({ errors: 'Geolocation is not supported by this browser.'});
+      console.log('error');
     }
+    // console.log('getting location');
   }
+  getWeather() {
+    const { lat, lon } = this.state.location;
+    const url = `https://api.darksky.net/forecast/${API_KEY}/${lat},${lon}`;
+    // console.log(url);
+    fetchJsonp(url)
+      .then(res => res.json())
+      .then(json => {
+        console.log(json);
+      })
+      .catch(error => console.log(error));    
+  }
+  // componentDidMount() {
+  //   setInterval(() => console.log('test'), 3000);
+  //   const { lat, lon } = this.state.location;
+  //   const url = `https://api.darksky.net/forecast/${API_KEY}/${lat},${lon}`;
+  //   // fetchJsonp()
+  //   console.log(lat, lon, this.state.location, this.state.location);
+  // }
 
   render() {
+    // this.getLocation();
+    if (this.state.location.lon) {
+      this.getWeather();
+    }
     return (
       <div>
         <h1>Weather App</h1>
+        <p>{this.state.errors}</p>
       </div>
     );
   }
