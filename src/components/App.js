97c5af4601ea9a5fb448e2070e49c678f1605b30
Ania Brajtkopf <aniabrajtkopf@gmail.com>
2017-04-24 11:25:29 +0200

    get location

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,34 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: true,
+      location: {},
+      errors: ''
+    };
+  }
+
+  componentWillMount() {
+    let lat = null, lon = null;
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        lat = position.coords.latitude;
+        lon = position.coords.longitude;
+        this.setState({
+          location: {
+            lat,
+            lon
+          }
+        });
+      });
+    } else {
+      this.setState({ errors: 'Geolocation is not supported by this browser.'});
+    }
+  }
+
   render() {
     return (
       <div>
