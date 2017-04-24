4bffbb7975aa477a3df9c3f018f6b8cdd8bbd0ed
Ania Brajtkopf <aniabrajtkopf@gmail.com>
2017-04-24 11:14:50 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './styles/styles.css';
+
+import App from './components/App';
+
+render(<App />, document.getElementById('root'));
